780f89167acde7c005692749d81bcd65e815c971
triplesecteam <163592632+triplesecteam@users.noreply.github.com>
2024-03-29 21:50:29 -0700

    script for decompilation with Ghidra

--- ghidrascript.py
+++ ghidrascript.py
@@ -1,3 +1,7 @@
+
+
+from ghidra.app.decompiler import DecompInterface
+from ghidra.util.task import ConsoleTaskMonitor
 def extract_binary_info():
     current_program = getCurrentProgram()
     print(type(current_program))
@@ -18,11 +22,40 @@ def extract_function_info(func_name):
             print(type(func.getBody()))
             print(func.getBody().getMinAddress())
             print(func.getBody().getMaxAddress())
+def decompile_function(function_name):
+    func_address = return_function_addr(function_name)
+    current_program = getCurrentProgram()
+    decomp_interface = DecompInterface()
+    decomp_interface.openProgram(current_program)
+    
+    # Get the function at the address
+    function = getFunctionAt(func_address)
 
+    if function:
+        # Decompile the function
+        decomp_results = decomp_interface.decompileFunction(function, 0,ConsoleTaskMonitor())
+        
+        if decomp_results.decompileCompleted():
+            # Get the decompiled code
+            decompiled_code = decomp_results.getDecompiledFunction().getC()
+            print(decompiled_code) 
+
+
+
+def return_function_addr(func_name):
+    current_program = getCurrentProgram()
+    fm = current_program.getFunctionManager()
+    funcs = fm.getFunctions(True)
+    for func in funcs:
+        #print(type(func))
+        if func.getName() == func_name:
+            return func.getEntryPoint()
 
 
 if __name__ == "__main__":
     args = getScriptArgs()
     if args:
         function_name = args[0]
-        extract_function_info(function_name)
+        decompile_function(function_name)
+        
+        
