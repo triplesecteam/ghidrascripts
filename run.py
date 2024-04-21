83ceac091657c162dab5bcb084070f36c3a37c62
triplesecteam <163592632+triplesecteam@users.noreply.github.com>
2024-04-20 22:56:28 -0700

    Update run.py

--- run.py
+++ run.py
@@ -3,6 +3,6 @@ headlessanalyzer_path = "/workspaces/tutorial1/ghidra_11.0_PUBLIC/support/analyz
 project_directory_path = "/workspaces/tutorial1/temp/"
 binaryPath = "/workspaces/tutorial1/CWE121_Stack_Based_Buffer_Overflow__char_type_overrun_memcpy_01.out"
 binaryname = "CWE121_Stack_Based_Buffer_Overflow__char_type_overrun_memcpy_01.out"
-function_name = "CWE121_Stack_Based_Buffer_Overflow__char_type_overrun_memcpy_01_bad"
-command = [headlessanalyzer_path, project_directory_path, binaryname ,  '-deleteProject', '-import', binaryPath, '-postscript', 'ghidrascript.py', f"{function_name}" ]
+
+command = [headlessanalyzer_path, project_directory_path, binaryname ,  '-deleteProject', '-import', binaryPath, '-postscript', 'ghidrascript.py']
 subprocess.run(command, check=True)
